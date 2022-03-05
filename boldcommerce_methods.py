b3808aaf30590260446af6b486ef3aa7e59c6831
Jenny Ischakov R <jenny.ischakov@gmail.com>
2022-03-05 15:56:15 -0600

    Add files via upload

--- boldcommerce_methods.py
+++ boldcommerce_methods.py
@@ -18,20 +18,20 @@ import boldcommerce_locators as locators
 # ================================================= Driver
 
 # driver - headless cloud running
-options = Options()
-options.add_argument("--headless")
-options.add_argument("window-size=1400,1500")
-options.add_argument("--disable-gpu")
-options.add_argument("--no-sandbox")
-options.add_argument("start-maximized")
-options.add_argument("enable-automation")
-options.add_argument("--disable-infobars")
-options.add_argument("--disable-dev-shm-usage")
+# options = Options()
+# options.add_argument("--headless")
+# options.add_argument("window-size=1400,1500")
+# options.add_argument("--disable-gpu")
+# options.add_argument("--no-sandbox")
+# options.add_argument("start-maximized")
+# options.add_argument("enable-automation")
+# options.add_argument("--disable-infobars")
+# options.add_argument("--disable-dev-shm-usage")
 
-driver = webdriver.Chrome(options=options)
+# driver = webdriver.Chrome(options=options)
 
 # driver - local running
-# driver = webdriver.Chrome(executable_path='C:\chromedriver.exe')
+driver = webdriver.Chrome(executable_path='C:\chromedriver.exe')
 
 action = ActionChains(driver)
 
